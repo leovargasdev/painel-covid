1c2af0b74138c51fc9c5ca42fe67264b26bf796c
LeeonardoVargas <leu1607@hotmail.com>
2020-08-31 16:40:28 -0300

    Ajustes nas referencias

--- gatsby-node.js
+++ gatsby-node.js
@@ -520,7 +520,7 @@ exports.createPages = async ({ graphql, actions }) => {
 
   // const minValue = [confirmedsChapeco.length, confirmedsMaravilha.length, confirmedsMiguel.length, confirmedsXanxere.length].sort()[0];
   const minValue = confirmedsChapeco.length;
-
+  console.log(confirmedsChapeco.length, confirmedsMaravilha.length, confirmedsMiguel.length, confirmedsXanxere.length);
   confirmedsChapeco.forEach((confimed, index) => {
     allDataCases.confirmeds.push(Object.assign(confimed, confirmedsMaravilha[index], confirmedsMiguel[index], confirmedsXanxere[index]));
     allDataCases.recovereds.push(Object.assign(recoveredsChapeco[index], recoveredsMaravilha[index], recoveredsMiguel[index], recoveredsXanxere[index]));
