43a4d01e3150caab0765c846231f416f704e9233
LeeonardoVargas <leu1607@hotmail.com>
2020-09-16 22:14:56 -0300

    Atualizando

--- gatsby-node.js
+++ gatsby-node.js
@@ -519,7 +519,7 @@ exports.createPages = async ({ graphql, actions }) => {
   };
 
   // const minValue = [confirmedsChapeco.length, confirmedsMaravilha.length, confirmedsMiguel.length, confirmedsXanxere.length].sort()[0];
-  const minValue = confirmedsChapeco.length - 3;
+  const minValue = confirmedsChapeco.length;
   console.log('SADASDASDASD', minValue);
   // console.log(confirmedsChapeco.length, confirmedsMaravilha.length, confirmedsMiguel.length, confirmedsXanxere.length);
   confirmedsChapeco.forEach((confimed, index) => {
